9731a4fe410a10704eed61ff3db0a30b12e65f65
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-05-27 14:04:33 -0400

    JM-017: remove unused code in main

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -6,23 +6,7 @@ package Main;
 public class Main
 {
     public static void main( String[] args ) {
-//        testNumberToWords();
 
-    }
-
-    public static void testNumberToWords() {
-//        System.out.println(NumberToWords.getDigitCount(123));
-//        System.out.println(NumberToWords.getDigitCount(1));
-//        System.out.println(NumberToWords.getDigitCount(12));
-//        System.out.println(NumberToWords.reverse(123));
-//        System.out.println(NumberToWords.reverse(1));
-//        System.out.println(NumberToWords.reverse(120));
-//        System.out.println(NumberToWords.reverse(102));
-//        System.out.println(NumberToWords.reverse(-102));
-        NumberToWords.numberToWords(123);
-        NumberToWords.numberToWords(1230);
-        NumberToWords.numberToWords(1010);
-        NumberToWords.numberToWords(-1230);
     }
 
 }
