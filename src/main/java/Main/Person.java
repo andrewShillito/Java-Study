e275ce0b6646e474303befa29cec73da68d1bbf5
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2019-06-04 19:05:01 -0400

    Closes #3: Person class

---
+++ src/main/java/Main/Person.java
@@ -0,0 +1,48 @@
+package Main;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age > 100 || age < 0) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public boolean isTeen() {
+        return age > 12 && age < 20;
+    }
+
+    public String getFullName() {
+        if (firstName.equals("") && lastName.equals("")) {
+            return "";
+        } else {
+            return firstName.equals("") ? lastName : lastName.equals("") ? firstName : firstName + " " + lastName;
+        }
+    }
+}
